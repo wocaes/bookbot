e6e5a0e30b501f8bdd34240f1b6f5454d8193bfd
wocaes_seacow <wocaesseacow@gmail.com>
2025-07-24 18:00:58 -0700

    C: redefined word_count to be simpler; removed junk comments

--- main.py
+++ main.py
@@ -4,25 +4,13 @@ def get_book_text(file_path):
 
 
 def word_count(book_text):
-    counter = 0
-    with open(book_text) as f:
-        words = f.read() 
-        num_words = words.split()
-        for word in num_words:
-            counter += 1
-    return counter
-
-"""def main():
-    book_text = "books/frankenstein.txt"
-    print(word_count(book_text))
-
-main()"""
-
-            
+    words = book_text.split() 
+    return len(words)
+    
     
 def main():
     file_path = "books/frankenstein.txt"
-    book_text = "books/frankenstein.txt"
+    book_text = get_book_text(file_path)
     print(f"{word_count(book_text)} words found in the document")
 
 main()
