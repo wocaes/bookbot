7bca01f813748d72b5ad772a4d236b18e40b90e8
wocaes_seacow <wocaesseacow@gmail.com>
2025-07-24 23:27:48 -0700

    F: finalized sorting functions; imported into main.py

--- stats.py
+++ stats.py
@@ -18,3 +18,19 @@ def letter_count(book_text):
     return dict_count
 
 
+#returns a sorted list of dictionaries
+def sort_on(dict_count):
+    return dict_count["num"]
+
+def sorted_dict(dict_count):
+    final_list = []
+    for char in dict_count:
+        char_name = char
+        char_count = dict_count[char]
+        sorted_dict = {"char": char, "num": dict_count[char]}
+        final_list.append(sorted_dict)
+    final_list.sort(reverse=True, key=sort_on)
+    return final_list
+
+
+    
