ea77168bba532d09fc47a92123d446915b27f9f2
wocaes_seacow <wocaesseacow@gmail.com>
2025-07-24 17:50:50 -0700

    B: word_count function; changed output to number of words

--- main.py
+++ main.py
@@ -2,8 +2,27 @@ def get_book_text(file_path):
     with open(file_path) as f:
         return f.read()
 
+
+def word_count(book_text):
+    counter = 0
+    with open(book_text) as f:
+        words = f.read() 
+        num_words = words.split()
+        for word in num_words:
+            counter += 1
+    return counter
+
+"""def main():
+    book_text = "books/frankenstein.txt"
+    print(word_count(book_text))
+
+main()"""
+
+            
+    
 def main():
     file_path = "books/frankenstein.txt"
-    print(get_book_text(file_path))
+    book_text = "books/frankenstein.txt"
+    print(f"{word_count(book_text)} words found in the document")
 
 main()
