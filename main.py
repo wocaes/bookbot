8d9065fa0663a896a67cd828c2cb405e06a6ba5c
wocaes_seacow <wocaesseacow@gmail.com>
2025-07-24 21:46:08 -0700

    E: letter_count function completed

--- main.py
+++ main.py
@@ -1,12 +1,17 @@
+from stats import word_count
+
+from stats import letter_count
+
+
 def get_book_text(file_path):
     with open(file_path) as f:
         return f.read()
 
-from stats import word_count    
 
 def main():
     file_path = "books/frankenstein.txt"
     book_text = get_book_text(file_path)
     print(f"{word_count(book_text)} words found in the document")
+    print(letter_count(book_text))
 
 main()
--- stats.py
+++ stats.py
@@ -1,3 +1,20 @@
+# counts words
+
 def word_count(book_text):
     words = book_text.split() 
     return len(words)
+
+# counts letters
+
+def letter_count(book_text):
+    book_text = book_text.lower()
+    dict_count = {}
+    
+    for char in book_text:
+        if char in dict_count:
+            dict_count[char] += 1
+        else:
+            dict_count[char] = 1
+    return dict_count
+
+
