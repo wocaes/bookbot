6a93fd4bc8e56aa5f5a79ae412924c5e0046c7ea
wocaes_seacow <wocaesseacow@gmail.com>
2025-07-25 23:57:08 -0700

    H: sys.argv finished; code works

--- main.py
+++ main.py
@@ -1,15 +1,25 @@
+import sys
+
 from stats import word_count
 
 from stats import letter_count
 
 from stats import sorted_dict
 
+#define CLI arguments
+args = sys.argv
+
+
 def get_book_text(file_path):
     with open(file_path) as f:
         return f.read()
 
 def main():
-    file_path = "books/frankenstein.txt"
+    #instruct how to use
+    if len(sys.argv) < 2:
+        print("Usage: python3 main.py <path_to_book>")
+        sys.exit(1)
+    file_path = args[1]
     book_text = get_book_text(file_path)
     print("============ BOOKBOT ============")
     print(f"Analyzing book found at {file_path}...")
